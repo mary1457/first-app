d78744baf0b926a13d19ffbe3140dda8565de240
mary1457 <piabaldoin@gmail.com>
2024-08-20 13:12:38 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+import ButtonComponent from './components/ButtonComponent';
+import ImageComponent from './components/ImageComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <ButtonComponent text="Save"> </ButtonComponent>
+        <ImageComponent src="https://placedog.net/500" alt="Dog" />
+      </header>
+    
+    </div>
+  );
+}
+
+export default App;
